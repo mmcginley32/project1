dd7edfea62a51f58564c925a1c9e4cde8ff29eaf
Mason McGinley <masonmcginley32@gmail.com>
2020-08-04 19:10:35 -0600

    Function for current weather

---
+++ Assets/script.js
@@ -0,0 +1,19 @@
+const apiKey = "0765d126b0f6a7eb158764d733ae5823";
+var currWeatherDiv = $("#");
+var forecastDiv = $("#");
+var citiesArray;
+
+function returnCurrentWeather(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function(response){
+        let currTime = new Date(response.dt*1000);
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        currWeatherDiv.html(`
+        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+        <p>Temperature: ${response.main.temp} &#176;C</p>
+        `)
+        createHistoryButton(response.name);
+    })
+};
