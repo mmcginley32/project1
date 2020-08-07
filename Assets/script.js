f408e100b9945c2e7abefd6fdf62e8a5210044ee
Christal Vigil <c.vigil27@gmail.com>
2020-08-06 20:14:21 -0600

    updating

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,43 @@
 
+//create variable to hold API key 
 const apiKey = "0765d126b0f6a7eb158764d733ae5823";
-var currWeatherDiv = $("#");
-var forecastDiv = $("#");
-var citiesArray;
+
+
+//create variables that bring in city values selected by user 
+let startCity = $("#start-city"); 
+let endCity = $("#end-city"); 
+let stopCity =$("#stop-city");
+console.log(startCity, endCity, stopCity); //logging to make sure code is functioning 
+
+
+// var currWeatherDiv = $("#");
+// var forecastDiv = $("#");
+
+//create an array for the start, end and stop city 
+var citiesArray = [startCity, endCity, stopCity];
+console.log(citiesArray); 
+
+
+// Click event for route button 
+$("#submit").click(function() {
+  event.preventDefault();
+  let cityName = $("#start-city").val();
+  let cityName2 = $("#end-city").val();
+  returnCurrentWeather(cityName, cityName2);
+  console.log(cityName, cityName2); 
+});
+
+// Click event for add button 
+$("#submit").click(function() {
+  event.preventDefault();
+  let cityName3 = $("#stop-city").val();
+  returnCurrentWeather(cityName3);
+  console.log(cityName3); 
+});
 
 
 function returnCurrentWeather(cityName) {
-     let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${coordinates.lat}&lon=${coordinates.lon}&APPID=${apiKey}`;;
+     let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${coordinates.lat}&lon=${coordinates.lon}&APPID=${apiKey}`;
 
     $.get(queryURL).then(function(response){
         let currTime = new Date(response.dt*1000);
@@ -20,91 +51,88 @@ function returnCurrentWeather(cityName) {
     })
 };
     citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches")) || [];
+ 
+    
+
+// // //create variables that will bring in the input values selected by user 
+// // let startCity = $("#start-city"); 
+// // let endCity = $("#end-city"); 
+// // let stopCity =$("#stop-city");
+// // console.log(startCity, endCity, stopCity); //logging to make sure code is functioning 
 
 
 
-//create empty arrays for input values selected by user 
-let startCity = $("#start-city"); 
-let endCity = $("#end-city"); 
-let stopCity =$("#stop-city");
-console.log(startCity, endCity, stopCity);
+// //code functionality that will execute once the route or add stop buttons are clicked 
+// let addStart = (ev) => {
+//   ev.preventDefault(); //prevents the form from submitting 
 
-
-
-//code functionality that will execute once the route or add stop buttons are clicked 
-let addStart = (ev) => {
-  ev.preventDefault(); //prevents the form from submitting 
-
-  let inputStart = document.getElementById('start-city').value; //lines 17-19: grabbing  user input values and assigning them to variables 
-  let inputEnd = document.getElementById('end-city').value; 
-  let inputStop = document.getElementById('stop-city').value; 
+//   let inputStart = document.getElementById('start-city').value; //lines 17-19: grabbing  user input values and assigning them to variables 
+//   let inputEnd = document.getElementById('end-city').value; 
+//   let inputStop = document.getElementById('stop-city').value; 
  
 
-  document.querySelector('form').reset(); //reset/clear the form for the next selected cities 
+//   document.querySelector('form').reset(); //reset/clear the form for the next selected cities 
 
-  console.log('Added:' , startCity); //console logging array values 
-  console.log('Added:', endCity); 
-  console.log('Added:', stopCity); 
+//   console.log('Added:' , startCity); //console logging array values 
+//   console.log('Added:', endCity); 
+//   console.log('Added:', stopCity); 
 
-  // let selectedStart = document.querySelector('createcontainer'); do we want to have selected cities for start, end, stop in appear on the page?
-  // selectedStart.textContent = (startCity); 
+//   // let selectedStart = document.querySelector('createcontainer'); do we want to have selected cities for start, end, stop in appear on the page?
+//   // selectedStart.textContent = (startCity); 
 
-  //locally storing input values and end of addStart execution 
-  localStorage.setItem('startCity', JSON.stringify(startCity) ); 
-  localStorage.setItem('endCity', JSON.stringify(endCity) ); 
-  localStorage.setItem('stopcity', JSON.stringify(stopCity) ); 
-}
+//   //locally storing input values and end of addStart execution 
+//   localStorage.setItem('startCity', JSON.stringify(startCity) ); 
+//   localStorage.setItem('endCity', JSON.stringify(endCity) ); 
+//   localStorage.setItem('stopcity', JSON.stringify(stopCity) ); 
+// }
 
 
-//add event listener for route and passing through the function addStart
-document.addEventListener('DOMContentLoaded', ()=> {
-  document.getElementById('route').addEventListener('click', addStart);
-});
+// //add event listener for route and passing through the function addStart
+// document.addEventListener('DOMContentLoaded', ()=> {
+//   document.getElementById('route').addEventListener('click', addStart);
+// });
 
-//add event listener for add and passing through the function addStart
-document.addEventListener('DOMContentLoaded', ()=> {
-  document.getElementById('add').addEventListener('click', addStart);
-});
+// //add event listener for add and passing through the function addStart
+// document.addEventListener('DOMContentLoaded', ()=> {
+//   document.getElementById('add').addEventListener('click', addStart);
+// });
 
 //add event 
-$('.btn-primary').on('click', function() {
-  var startAPI = $('#start-city').val(); 
-  var endAPI = $('#end-city').val(); 
-  var stopAPI = $('#stop-city').val(); 
-  searchTrip (startAPI, endAPI,stopAPI); 
-  console.log(startAPI, endCity, stopAPI); 
-});
+// $('.btn-primary').on('click', function() {
+//   var startAPI = $('#start-city').val(); 
+//   var endAPI = $('#end-city').val(); 
+//   var stopAPI = $('#stop-city').val(); 
+//   searchTrip (startAPI, endAPI,stopAPI); 
+//   console.log(startAPI, endCity, stopAPI); 
+// });
 
 
-//Function to execute the API call once buttons are clicked 
-function searchTrip(cityAPI) {
-      var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityAPI + '&appid=ce3b9593e61b336933f1777b5554991c';
-  //  var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + startAPI + '&' + endCity + '&' + stopAPI + '&appid=ce3b9593e61b336933f1777b5554991c';
+// already have function --- keep in case needed  
 
-$.ajax ({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    $("#end-city").text(JSON.stringify(response)); 
+// Function to execute the API call once buttons are clicked 
 
-  }); 
-};
+// function searchTrip(cityAPI) {
+//       var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityAPI + '&appid=ce3b9593e61b336933f1777b5554991c';
+//    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + startAPI + '&' + endCity + '&' + stopAPI + '&appid=ce3b9593e61b336933f1777b5554991c';
+
+// $.ajax ({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//     $("#end-city").text(JSON.stringify(response)); 
+
+//   }); 
+// };
 
 
-
-
-//returnCurrentWeather("Toronto");
-//returnSearchTrip("Toronto");
-
-//$("#submit").click(function() {
-//    event.preventDefault();
-//    let cityName = $("#start-city").val();
-//    let cityName2 = $("#stop-city").val();
-//    returnCurrentWeather(cityName, cityName2);
-//    returnSearchTrip(cityName, cityName2);
-//});
 
 
 
 // 0d67df869da6450e2d0d3147f8e85294701ac392
+
+
+// call searchtrip for start, end and stop if there is a value (if not empty string then call searchtrip startAPI) return current weather called within search Trip function and search trip function can be called for each city 
+
+// start city, end city and add city into array 
+
