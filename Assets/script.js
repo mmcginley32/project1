cdd20702cda7f5dab15647dcc4a2d9629a879a5e
Christal Vigil <c.vigil27@gmail.com>
2020-08-08 10:33:15 -0600

    updating

--- Assets/script.js
+++ Assets/script.js
@@ -98,22 +98,22 @@ function stopCurrentWeather(api) {
 
 //////////////////UV CALL FOR ONE LOCATION: FUNCTION FOR API /////////////////////
 
-// function returnCurrentWeather(coordinates) {
-//   let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${coordinates.lat}&lon=${coordinates.lon}&APPID=${apiKey}`;
+function returnCurrentWeather(coordinates) {
+  let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${coordinates.lat}&lon=${coordinates.lon}&APPID=${apiKey}`;
 
-//  $.get(queryURL).then(function(response){
-//      let currTime = new Date(response.dt*1000);
-//      let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+ $.get(queryURL).then(function(response){
+     let currTime = new Date(response.dt*1000);
+     let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
 
-//      currWeatherDiv.html(`
-//      <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
-//      <p>Temperature: ${response.main.temp} &#176;C</p>
-//      `)
-//      createHistoryButton(response.name);
-//      console.log(response); // console logging 
-//  })
-// };
-//  citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches")) || [];
+     currWeatherDiv.html(`
+     <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+     <p>Temperature: ${response.main.temp} &#176;C</p>
+     `)
+     createHistoryButton(response.name);
+     console.log(response); // console logging 
+ })
+};
+ citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches")) || [];
 
 /////////////////////CLICK EVENTS FOR WEATHER//////////////////////////////
 
